69f1654ba3d08be4f19bc1541025b3c2585d91c4
Joice Ferreira <34250807+Joiceff@users.noreply.github.com>
2025-09-05 16:50:07 -0300

    Update uol-termos.cy.js

--- cypress/e2e/uol-termos.cy.js
+++ cypress/e2e/uol-termos.cy.js
@@ -1,6 +1,6 @@
 describe('Validação Termos de Uso - UOL', () => {
   it('Deve acessar os termos de uso e validar a última atualização', () => {
-    cy.visit('https://www.uol.com.br/termos-de-uso');
+    cy.visit('https://noticias.uol.com.br/regras/termos-de-uso/');
     cy.contains(/Última atualização/i).should('exist');
 
     // Capturar a data no log
